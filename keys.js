0264153c43bb75ab920b6a0c81588a160355d9d3
garret <garretgolledge@gmail.com>
2018-04-09 10:09:02 -0700

    worked on adding tweets

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.QwYdXk2WWp6DUkYSTzbu9M2kd, 
-  consumer_secret: process.env.NQqHkVrTnJHUbO0WBwSbsrMd29VmdrCxBCASj1VXu34fMXjoPE,
-  access_token_key: process.env.bg19AuNF37o9zZUex98uqfyjhEv, 
-  access_token_secret: process.env.BqkKS37ffTuWnOBqzLRl4n3Cp3JwE8bKprRtY94yPCjYn
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env. ef7fa051eea74358b509ff49be89ad8a, 
-  secret: process.env.ac6d9fcb3fd74f1d9afac3419bc4827e, 
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET 
 };
